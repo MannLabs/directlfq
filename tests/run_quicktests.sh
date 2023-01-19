5e71bb11b8b83f432c054d030c9a2d51b612ca01
ammarcsj <70114795+ammarcsj@users.noreply.github.com>
2023-01-19 16:06:58 +0100

    fixes for github actions

--- tests/run_quicktests.sh
+++ tests/run_quicktests.sh
@@ -1,5 +1,5 @@
 conda activate directlfq
 pip install wget
 python download_testfiles.py quicktest
-jupyter nbconvert --to notebook --execute ./quicktests/run_pipeline_w_different_input_formats.ipynb
+jupyter nbconvert --NotebookApp.kernel_manager_class=notebook.services.kernels.kernelmanager.AsyncMappingKernelManager --to notebook --execute ./quicktests/run_pipeline_w_different_input_formats.ipynb
 conda deactivate
