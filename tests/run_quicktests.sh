a59aec17e53431daaec07ebd34992a31539e07fc
ammarcsj <70114795+ammarcsj@users.noreply.github.com>
2023-01-20 15:53:47 +0100

    change to papermill

--- tests/run_quicktests.sh
+++ tests/run_quicktests.sh
@@ -1,5 +1,8 @@
 conda activate directlfq
 pip install wget
 python download_testfiles.py quicktest
-jupyter nbconvert --NotebookApp.kernel_manager_class=notebook.services.kernels.kernelmanager.AsyncMappingKernelManager --to notebook --execute ./quicktests/run_pipeline_w_different_input_formats.ipynb
+cd quicktests
+echo "Script executed from: ${PWD}"
+papermill run_pipeline_w_different_input_formats.ipynb run_pipeline_w_different_input_formats.nbconvert.ipynb
+cd ..
 conda deactivate
