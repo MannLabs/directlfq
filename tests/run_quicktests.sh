458745fc817a341d7f0dea1f3d75b8726d9f9131
ammarcsj <70114795+ammarcsj@users.noreply.github.com>
2023-01-23 20:37:52 +0100

    up

--- tests/run_quicktests.sh
+++ tests/run_quicktests.sh
@@ -4,7 +4,6 @@ pip install papermill
 python download_testfiles.py quicktest
 cd quicktests
 echo "Script executed from: ${PWD}"
-jupyter nbconvert --to script run_pipeline_w_different_input_formats.ipynb
-python run_pipeline_w_different_input_formats.py
+jupyter execute run_pipeline_w_different_input_formats.ipynb
 cd ..
 conda deactivate
