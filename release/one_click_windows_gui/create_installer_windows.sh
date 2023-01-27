427de044eb2a36c4d933a64531f105310932e595
ammarcsj <70114795+ammarcsj@users.noreply.github.com>
2023-01-27 14:56:16 +0100

    up

--- release/one_click_windows_gui/create_installer_windows.sh
+++ release/one_click_windows_gui/create_installer_windows.sh
@@ -18,11 +18,11 @@ python setup.py sdist bdist_wheel
 cd release/one_click_windows_gui
 # Make sure you include the required extra packages and always use the stable or very-stable options!
 pip install "../../dist/directlfq-0.2.0-py3-none-any.whl[stable]"
-pip install jinja2==3.0
 
 # Creating the stand-alone pyinstaller folder
 pip install pyinstaller==4.10
 pyinstaller ../pyinstaller/directlfq.spec -y
+pip install jinja2==3.0
 conda deactivate
 
 # If needed, include additional source such as e.g.:
