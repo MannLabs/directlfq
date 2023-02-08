f45f212b1d4d25bf7c9793bcb411126187a31f66
ammarcsj <70114795+ammarcsj@users.noreply.github.com>
2023-02-08 12:10:43 +0100

    change actions workflows

--- misc/loose_pip_install.sh
+++ misc/loose_pip_install.sh
@@ -1,5 +1,5 @@
 conda create -n directlfq python=3.8 -y
 conda activate directlfq
-pip install -e '../.[development]'
+pip install -e '../.[development, gui]'
 directlfq
 conda deactivate
--- misc/stable_pip_install.sh
+++ misc/stable_pip_install.sh
@@ -1,5 +1,5 @@
 conda create -n directlfq python=3.8 -y
 conda activate directlfq
-pip install -e '../.[stable,development-stable]'
+pip install -e '../.[stable,development-stable, gui-stable]'
 directlfq
 conda deactivate
