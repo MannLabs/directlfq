e72c9d5944e2abe5da75916e80757b60753f441b
ammarcsj <70114795+ammarcsj@users.noreply.github.com>
2023-02-22 14:21:39 +0100

    remove double dot

--- directlfq/lfq_manager.py
+++ directlfq/lfq_manager.py
@@ -62,7 +62,7 @@ def prepare_input_filename(input_file):
 
 def get_outfile_basename(input_file, input_type_to_use, selected_proteins_file, deactivate_normalization,filename_suffix):
     outfile_basename = input_file
-    outfile_basename += "" if input_type_to_use is None else f".{input_type_to_use}."
+    outfile_basename += "" if input_type_to_use is None else f".{input_type_to_use}"
     outfile_basename += ".selected_proteins" if selected_proteins_file is not None else ""
     outfile_basename += ".no_norm" if deactivate_normalization else ""
     outfile_basename += filename_suffix
