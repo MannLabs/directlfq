75679ae5986101d3153088139da768705a61dc4e
ammarcsj <70114795+ammarcsj@users.noreply.github.com>
2023-02-28 17:44:04 +0100

    add gui

--- release/pypi/install_pypi_wheel.sh
+++ release/pypi/install_pypi_wheel.sh
@@ -1,5 +1,5 @@
 conda create -n directlfq_pip_test python=3.8 -y
 conda activate directlfq_pip_test
-pip install "directlfq[stable]"
+pip install "directlfq[stable, gui]"
 directlfq
 conda deactivate
