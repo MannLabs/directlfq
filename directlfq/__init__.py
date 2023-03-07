125744f932141baba1d279d4a00b6662bf1c1bdf
ammarcsj <70114795+ammarcsj@users.noreply.github.com>
2023-03-07 15:26:22 +0100

    Bump version: 0.2.8 → 0.2.9

--- directlfq/__init__.py
+++ directlfq/__init__.py
@@ -2,7 +2,7 @@
 
 
 __project__ = "directlfq"
-__version__ = "0.2.8"
+__version__ = "0.2.9"
 __license__ = "Apache"
 __description__ = "An open-source Python package of the AlphaPept ecosystem"
 __author__ = "Mann Labs"
