e2cdc0c99b5cfa6f8acc30b9baac8d6a48a0e08d
ammarcsj <70114795+ammarcsj@users.noreply.github.com>
2024-04-08 18:21:51 +0200

    add global variable to check for read only bug

--- directlfq/config.py
+++ directlfq/config.py
@@ -1,4 +1,5 @@
 import logging
+import pandas as pd
 
 
 def setup_logging():
@@ -30,3 +31,26 @@ def set_compile_normalized_ion_table(compile_normalized_ion_table = True):
     global COMPILE_NORMALIZED_ION_TABLE
     COMPILE_NORMALIZED_ION_TABLE = compile_normalized_ion_table
 
+##########################
+COPY_NUMPY_ARRAYS_DERIVED_FROM_PANDAS = False
+
+def check_wether_to_copy_numpy_arrays_derived_from_pandas():
+    global COPY_NUMPY_ARRAYS_DERIVED_FROM_PANDAS
+    try:
+        _manipulate_numpy_array_without_copy()
+        COPY_NUMPY_ARRAYS_DERIVED_FROM_PANDAS = False
+    except:
+        COPY_NUMPY_ARRAYS_DERIVED_FROM_PANDAS = True
+
+def _manipulate_numpy_array_without_copy():
+    
+    protein_profile_df = pd.DataFrame({
+    'ProteinA': [10, 20, 30, 40],
+    'ProteinB': [15, 25, 35, 45],
+    'ProteinC': [20, 30, 40, 50]
+    }, index=['Sample1', 'Sample2', 'Sample3', 'Sample4'])
+    
+    protein_profile_df = protein_profile_df.iloc[1:3]
+    protein_profile_numpy = protein_profile_df.to_numpy(copy=False)
+
+    protein_profile_numpy[0] = protein_profile_numpy[0] +2
