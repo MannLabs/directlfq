b98892b8c10c225090ed0944c4ce85dc3c2d13c7
ammarcsj <70114795+ammarcsj@users.noreply.github.com>
2024-08-12 17:33:00 +0200

    add cli run to quicktests

--- tests/run_quicktests.sh
+++ tests/run_quicktests.sh
@@ -4,5 +4,6 @@ python download_testfiles.py quicktest
 cd quicktests
 jupyter nbconvert --to script run_pipeline_w_different_input_formats.ipynb
 python run_pipeline_w_different_input_formats.py
+directlfq lfq -i ../../test_data/system_tests/quicktests/diann/shortened_input.tsv
 cd ..
 conda deactivate
