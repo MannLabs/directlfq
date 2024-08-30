79521f83b37d4118597f0aa4ecbde9c0c003a30d
mschwoerer <82171591+mschwoer@users.noreply.github.com>
2024-08-30 13:55:47 +0200

    adapt linux workflow

---
+++ release/linux/build_package_linux.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Build the install package for Linux.
+# This script must be run from the root of the repository after running build_installer_linux.sh
+
+PACKAGE_NAME=directlfq
+
+# BUILD_NAME is taken from environment variables, e.g. 'peptdeep-1.2.1-linux-x64'
+rm -rf ${BUILD_NAME}.deb
+
+# If needed, include additional source such as e.g.:
+# cp ../../directlfq/data/*.fasta dist/directlfq/data
+# WARNING: this probably does not work!!!!
+
+# Wrapping the pyinstaller folder in a .deb package
+mkdir -p dist/${BUILD_NAME}/usr/local/bin
+mv dist/${PACKAGE_NAME} dist/${BUILD_NAME}/usr/local/bin/${PACKAGE_NAME}
+mkdir dist/${BUILD_NAME}/DEBIAN
+cp control dist/${BUILD_NAME}/DEBIAN
+dpkg-deb --build --root-owner-group dist/${BUILD_NAME}/
