a9c3254244568f5652d1ab1a1baee7e93ddbbdd7
mschwoerer <82171591+mschwoer@users.noreply.github.com>
2024-08-30 16:15:56 +0200

    fix windows installer

--- release/windows/build_installer_windows.ps1
+++ release/windows/build_installer_windows.ps1
@@ -14,8 +14,10 @@ pip install pyinstaller
 pyinstaller release/pyinstaller/peptdeep.spec -y
 pip install jinja2==3.0
 
+ls dist
+ls release/windows/dist
 # for some reason, the installer builder expects the files here
-mv dist/* release/windows/dist
-mkdir release/windows/dist/peptdeep
-mv release/windows/dist/peptdeep.exe release/windows/dist/peptdeep
+#mv dist/* release/windows/dist
+#mkdir release/windows/dist/peptdeep
+#mv release/windows/dist/peptdeep.exe release/windows/dist/peptdeep
 
