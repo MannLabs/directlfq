086c941d6f85884fe375d8a10d230685cb1314d8
ammarcsj <70114795+ammarcsj@users.noreply.github.com>
2024-10-16 17:42:43 +0200

    allow longer download times

--- tests/run_ratio_tests.sh
+++ tests/run_ratio_tests.sh
@@ -4,7 +4,7 @@ cd ratio_tests
 pip install pytest
 pip install nbmake==1.5.3
 echo "Running ratio tests"
-pytest --nbmake systemtest_ratio_mixed_species1.ipynb --nbmake-timeout=1200
-pytest --nbmake systemtest_ratio_mixed_species2.ipynb --nbmake-timeout=1200
+pytest --nbmake systemtest_ratio_mixed_species1.ipynb --nbmake-timeout=1800
+pytest --nbmake systemtest_ratio_mixed_species2.ipynb --nbmake-timeout=1800
 cd ..
 conda deactivate
