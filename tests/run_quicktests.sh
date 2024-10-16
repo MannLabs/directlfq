53a2abde4c0b1eb893c292431038d707412c973b
ammarcsj <70114795+ammarcsj@users.noreply.github.com>
2024-10-16 14:15:30 +0200

    add alphabase install

--- tests/run_quicktests.sh
+++ tests/run_quicktests.sh
@@ -1,6 +1,7 @@
 conda activate directlfq
 pip install pytest
 pip install nbmake==1.5.3
+pip install alphabase>=1.2.1
 cd quicktests
 pytest --nbmake run_pipeline_w_different_input_formats.ipynb
 directlfq lfq -i ../../test_data/system_tests/quicktests/diann/shortened_input.tsv
