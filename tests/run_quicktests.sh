cb945d2181c6b34f44a20167fd678e3f2b09206a
ammarcsj <70114795+ammarcsj@users.noreply.github.com>
2024-10-16 14:01:07 +0200

    update quicktest download

--- tests/run_quicktests.sh
+++ tests/run_quicktests.sh
@@ -1,8 +1,6 @@
 conda activate directlfq
 pip install pytest
 pip install nbmake==1.5.3
-pip install wget
-python download_testfiles.py quicktest
 cd quicktests
 pytest --nbmake run_pipeline_w_different_input_formats.ipynb
 directlfq lfq -i ../../test_data/system_tests/quicktests/diann/shortened_input.tsv
