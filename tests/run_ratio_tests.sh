eb02cda51518d7d82fd7fb174ae42b98a5e9b6da
ammarcsj <70114795+ammarcsj@users.noreply.github.com>
2024-10-16 16:56:08 +0200

    fix paths

--- tests/run_ratio_tests.sh
+++ tests/run_ratio_tests.sh
@@ -4,8 +4,7 @@ cd ratio_tests
 pip install pytest
 pip install nbmake==1.5.3
 echo "Running ratio tests"
-cd tests/ratio_tests
 pytest --nbmake systemtest_ratio_mixed_species1.ipynb
 pytest --nbmake systemtest_ratio_mixed_species2.ipynb
-cd ../..
+cd ..
 conda deactivate
