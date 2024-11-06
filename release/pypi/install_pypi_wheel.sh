a73e7fe30a5f1f565f431f60a40af20b929dccef
mschwoerer <82171591+mschwoer@users.noreply.github.com>
2024-11-06 15:35:16 +0100

    remove old release workflow

--- release/pypi/install_pypi_wheel.sh
+++
@@ -1,6 +0,0 @@
-# TODO remove with old release workflow
-conda create -n directlfq_pip_test python=3.9 -y
-conda activate directlfq_pip_test
-pip install "directlfq[stable, gui]"
-directlfq
-conda deactivate
--- release/pypi/install_test_pypi_wheel.sh
+++
@@ -1,6 +0,0 @@
-# TODO remove with old release workflow
-conda create -n directlfq_pip_test python=3.9 -y
-conda activate directlfq_pip_test
-pip install --index-url https://test.pypi.org/simple/ --extra-index-url https://pypi.org/simple "directlfq[stable, gui]"
-directlfq
-conda deactivate
