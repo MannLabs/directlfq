22ede7d8fa5e28c9360e9035e25d120322832c21
mschwoerer <82171591+mschwoer@users.noreply.github.com>
2024-11-26 08:44:29 +0100

    get rid of duplicated import

--- tests/run_quicktests.sh
+++ tests/run_quicktests.sh
@@ -1,7 +1,5 @@
 conda activate directlfq
-pip install pytest
-pip install nbmake==1.5.3
-pip install alphabase>=1.2.1
+pip install -r requirements/requirements_development.txt
 cd quicktests
 pytest --nbmake run_pipeline_w_different_input_formats.ipynb
 directlfq lfq -i ../../test_data/system_tests/quicktests/diann/shortened_input.tsv
