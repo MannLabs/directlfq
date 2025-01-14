03758b295dcc7e788fdae7bd20b9250b38f30dc5
mschwoerer <82171591+mschwoer@users.noreply.github.com>
2025-01-14 15:34:18 +0100

    fix requirements

--- misc/stable_pip_install.sh
+++ misc/stable_pip_install.sh
@@ -1,5 +1,5 @@
 conda create -n directlfq python=3.9 -y
 conda activate directlfq
-pip install -e '../.[stable,development-stable, gui-stable]'
+pip install -e '../.[stable, gui-stable, development]'
 directlfq
 conda deactivate
