22eaea388469174f16ba6efc3db310e619ae3a4f
mschwoerer <82171591+mschwoer@users.noreply.github.com>
2025-01-16 17:22:28 +0100

    add build_wheel.sh

--- release/macos/build_installer_macos.sh
+++ release/macos/build_installer_macos.sh
@@ -3,15 +3,11 @@ set -e -u
 
 # Build the installer for MacOS.
 # This script must be run from the root of the repository.
+# Prerequisites: wheel has been build, e.g. using build_wheel.sh
 
-rm -rf dist build *.egg-info
 rm -rf dist_pyinstaller build_pyinstaller
 
-# Creating the wheel
-python -m build
-
-# substitute X.Y.Z-devN with X.Y.Z.devN
-WHL_NAME=$(echo "directlfq-0.3.1-dev0-py3-none-any.whl" | sed 's/\([0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*\)-dev\([0-9][0-9]*\)/\1.dev\2/g')
+WHL_NAME=$(cd dist && ls ./*.whl && cd ..)
 pip install "dist/${WHL_NAME}[stable,gui-stable]"
 
 # Creating the stand-alone pyinstaller folder
