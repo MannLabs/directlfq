38c9f916df97a282c78fa387ff6f191ba05c5152
null <82171591+mschwoer@users.noreply.github.com>
2025-01-16 16:23:36 +0000

    Bump version: 0.3.1-dev0 → 0.3.1-dev1

--- release/macos/build_installer_macos.sh
+++ release/macos/build_installer_macos.sh
@@ -11,7 +11,7 @@ rm -rf dist_pyinstaller build_pyinstaller
 python -m build
 
 # substitute X.Y.Z-devN with X.Y.Z.devN
-WHL_NAME=$(echo "directlfq-0.3.1-dev0-py3-none-any.whl" | sed 's/\([0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*\)-dev\([0-9][0-9]*\)/\1.dev\2/g')
+WHL_NAME=$(echo "directlfq-0.3.1-dev1-py3-none-any.whl" | sed 's/\([0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*\)-dev\([0-9][0-9]*\)/\1.dev\2/g')
 pip install "dist/${WHL_NAME}[stable,gui-stable]"
 
 # Creating the stand-alone pyinstaller folder
