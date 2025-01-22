ae27101a4cae1948a70ffaab2d40f02c507598fc
mschwoerer <82171591+mschwoer@users.noreply.github.com>
2025-01-22 11:43:49 +0100

    fix tests

--- tests/run_quicktests.sh
+++ tests/run_quicktests.sh
@@ -1,5 +1,5 @@
 conda activate directlfq
-pip install -r requirements/requirements_development.txt
+pip install -r ../requirements/requirements_development.txt
 cd quicktests
 pytest --nbmake run_pipeline_w_different_input_formats.ipynb
 directlfq lfq -i ../../test_data/system_tests/quicktests/diann/shortened_input.tsv
