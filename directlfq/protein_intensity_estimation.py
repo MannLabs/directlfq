ed59cc9673060c470da7f9bc023adc85afea8b7e
mschwoerer <82171591+mschwoer@users.noreply.github.com>
2026-06-16 14:48:50 +0200

    remove get_subdf

--- directlfq/protein_intensity_estimation.py
+++ directlfq/protein_intensity_estimation.py
@@ -5,7 +5,6 @@ __all__ = [
     "get_list_with_multiprocessing",
     "get_configured_multiprocessing_pool",
     "get_protein_workitems",
-    "get_normed_dfs",
     "get_ion_intensity_dataframe_from_list_of_shifted_peptides",
     "add_protein_names_to_ion_ints",
     "add_protein_name_to_ion_df",
@@ -13,9 +12,6 @@ __all__ = [
     "calculate_peptide_and_protein_intensities",
     "get_protein_profile_from_shifted_peptides",
     "get_list_with_protein_value_for_each_sample",
-    "OrphanIonRemover",
-    "OrphanIonsForDeletionSelector",
-    "IonCheckedForOrphan",
 ]
 
 import pandas as pd
@@ -117,20 +113,6 @@ def get_protein_workitems(
     )
 
 
-def get_normed_dfs(normed_df):
-    protein_names = normed_df.index.get_level_values(0).to_numpy()
-    ion_names = normed_df.index.get_level_values(1).to_numpy()
-    normed_array = normed_df.to_numpy()
-    indices_of_proteinname_switch = find_nameswitch_indices(protein_names)
-    results_list = [
-        get_subdf(
-            normed_array, indices_of_proteinname_switch, idx, protein_names, ion_names
-        )
-        for idx in range(len(indices_of_proteinname_switch) - 1)
-    ]
-
-    return results_list
-
 
 def find_nameswitch_indices(arr):
     change_indices = np.where(arr[:-1] != arr[1:])[0] + 1
@@ -144,17 +126,6 @@ def find_nameswitch_indices(arr):
     return start_indices
 
 
-def get_subdf(
-    normed_array, indices_of_proteinname_switch, idx, protein_names, ion_names
-):
-    start_switch = indices_of_proteinname_switch[idx]
-    end_switch = indices_of_proteinname_switch[idx + 1]
-    sub_array = normed_array[start_switch:end_switch]
-    index_sub_array = pd.MultiIndex.from_arrays(
-        [protein_names[start_switch:end_switch], ion_names[start_switch:end_switch]],
-        names=[config.PROTEIN_ID, config.QUANT_ID],
-    )
-    return pd.DataFrame(sub_array, index=index_sub_array)
 
 
 def get_list_with_sequential_processing(protein_workitems):
